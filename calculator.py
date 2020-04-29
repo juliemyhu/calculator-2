c69e75260858facdb047af74e8b5e84db4716ada
Julie Hu <jhu21495@gmail.com>
2020-04-29 23:08:28 +0000

    initial commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+while True:
+	token.split(',')
+	if the first token is "q":
+		quit
+	else: 
+		if the first token is "+":
+
