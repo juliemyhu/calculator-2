7eb3e74a2349d3f1f9d3af2083af5c9e694275b0
Julie Hu <jhu21495@gmail.com>
2020-04-29 23:46:43 +0000

    takes input as tokens

--- calculator.py
+++ calculator.py
@@ -3,15 +3,14 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-while True:
+while True
 	input_string = input("Enter your equation: ")
 	tokens = input_string.split(' ')
 	print (tokens)
 
 	if "q" in tokens:
-		break
+		break 
 
-	# else: 
-	# 	if token[0] is "+":
-	# 		pass
-
+	operand = token[0]
+	num1 = token[1]
+	num22 = token[2]
