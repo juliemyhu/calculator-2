91c4831230cf53780a9a2da8a5f1f0b5e8fa1d9f
Julie Hu <jhu21495@gmail.com>
2020-04-30 00:50:31 +0000

    Working Calculator

--- calculator.py
+++ calculator.py
@@ -3,14 +3,45 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-while True
-	input_string = input("Enter your equation: ")
-	tokens = input_string.split(' ')
-	print (tokens)
+pow 
 
-	if "q" in tokens:
+while True:
+	input_string = input("Enter your equation: ")
+	token = input_string.split(' ')
+	print (token)
+
+	token[1] = float(token[1])
+	token[2] = float(token[2])
+
+	answer = None
+
+	if "q" in token:
+		print ("calculator shutting down...")
 		break 
 
-	operand = token[0]
-	num1 = token[1]
-	num22 = token[2]
+	elif token[0] == "+":
+		answer = add(token[1],token[2])
+
+	elif token[0] == "-":
+		answer = subtract(token[1], token[2])
+	# pow 3 9 
+	elif token[0] == "*":
+		answer = multiply(token[1], token[2])
+
+	elif token[0] == "/":
+		answer = divide(token[1], token[2])
+
+	elif token[0] == "square":
+		answer = square(token[1])
+
+	elif token[0] == "cube":
+		answer = cube(token[1])
+
+	elif token[0] == "pow":
+		answer = power(token[1], token[2])
+
+	elif token[0] == "mod" or token[0] == "%":
+		answer = mod(token[1], token[2])
+
+
+	print(answer)
